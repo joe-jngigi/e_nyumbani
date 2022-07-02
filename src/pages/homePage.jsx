f8544b66e66ed437b84ea284f033312589efa8e5
SoftOps <josephngigi775@gmail.com>
2022-07-02 23:56:32 +0300

    e_nyumbani update four: Homepage

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,9 +1,10 @@
+import HeadSec from "../components/Home_section_one/headSection";
+
 const HomePage = () =>{
     return(
         <>
-        <div>This is the page for all the main
-            components
-        <button className="btn">User</button>
+        <div>
+            <HeadSec/>
         </div>
         </>
     )
