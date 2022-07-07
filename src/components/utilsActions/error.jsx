0760697805d277902a2e06f6335c40c0f501139b
SoftOps <josephngigi775@gmail.com>
2022-07-07 23:30:24 +0300

    deleted unwanted files

--- src/components/utilsActions/error.jsx
+++
@@ -1,9 +0,0 @@
-const Error = () => {
-  return (
-    <div className='section section-center text-center'>
-      <h2>There is a bug...Spray it!</h2>
-    </div>
-  )
-}
-
-export default Error
--- src/components/utilsActions/loading.jsx
+++
@@ -1,6 +0,0 @@
-const Loading = () =>{
-    return(
-        <div className="loading"></div>
-    )
-}
-export default Loading
